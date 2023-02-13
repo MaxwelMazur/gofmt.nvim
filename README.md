d46966676c8c37191a9a1065f592b51913965ab4
MaxwelMazur <maxwelbm@hotmail.com>
2023-02-13 20:26:35 -0300

    fix: teste de entendimento

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # gofmt.nvim
 
 gofmt.nvim é usada para formatar o código fonte escrito em Go. Ela torna o código mais fácil de ler e manter, além de ajudar a garantir que o código seja consistente com as boas práticas da linguagem.
+
+
+
+
--- lua/gofmt/init.lua
+++ lua/gofmt/init.lua
@@ -10,15 +10,6 @@ local function list_to_set(list)
   return set
 end
 
--- local function M.format_current_file()
---   local extension = vim.fn.expand("%:e")
---   if extension == "go" then
---     local fileName = vim.api.nvim_buf_get_name(0)
---     local command = "gofmt -w " .. fileName
---     os.execute(command)
---   end
--- end
-
 local function is_excluded_ft(opts)
   local ft = vim.api.nvim_buf_get_option(0, "filetype")
   local ft_set = list_to_set(opts.filetype_exclude)
@@ -46,33 +37,53 @@ local function reset_cursor_pos(pos)
 end
 
 function M.setup(opts)
+  print("escrevendo....")
+  -- local defaults = {
+  --   filetype_exclude = {},
+  -- }
 
-  local defaults = {
-    filetype_exclude = {},
-  }
+  -- opts = vim.tbl_extend("force", defaults, opts or {})
 
-  opts = vim.tbl_extend("force", defaults, opts or {})
+  -- local tidy_grp = vim.api.nvim_create_augroup("tidy", { clear = true })
 
-  local tidy_grp = vim.api.nvim_create_augroup("tidy", { clear = true })
+  -- vim.api.nvim_create_autocmd("BufWritePre", {
+  --   group = tidy_grp,
+  --   callback = function()
+  --     if is_excluded_ft(opts) then
+  --       return false
+  --     end
 
-  vim.api.nvim_create_autocmd("BufWritePre", {
-    group = tidy_grp,
-    callback = function()
-      if is_excluded_ft(opts) then
-        return false
-      end
+  --     local cursor_pos = vim.api.nvim_win_get_cursor(0)
 
-      local cursor_pos = vim.api.nvim_win_get_cursor(0)
+  --     -- delete trailing whitespace
+  --     vim.cmd([[:keepjumps keeppatterns %s/\s\+$//e]])
 
-      -- delete trailing whitespace
-      vim.cmd([[:keepjumps keeppatterns %s/\s\+$//e]])
+  --     -- delete lines @ eof
+  --     vim.cmd([[:keepjumps keeppatterns silent! 0;/^\%(\n*.\)\@!/,$d_]])
 
-      -- delete lines @ eof
-      vim.cmd([[:keepjumps keeppatterns silent! 0;/^\%(\n*.\)\@!/,$d_]])
-
-      reset_cursor_pos(cursor_pos)
-    end,
-  })
+  --     reset_cursor_pos(cursor_pos)
+  --   end,
+  -- })
 end
+
+function M.format_current_file(opts)
+  local extension = vim.fn.expand("%:e")
+  if extension == "go" then
+    local fileName = vim.api.nvim_buf_get_name(0)
+    local command = "gofmt -w " .. fileName
+    os.execute(command)
+  end
+end
+
+function M.escreve()
+  print("escrevendo....")
+end
+
+-- "MaxwelMazur/gofmt.nvim",
+-- config = function()
+--   require("gofmt").setup({
+--     filetype_exclude = { "markdown", "diff" },
+--   })
+-- end,
 
 return M
